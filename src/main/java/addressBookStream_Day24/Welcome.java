f9d5be8593152d6c199c5c5e0d915c505e47bba5
riteshkhunte05 <riteshkhunte05@gmail.com>
2022-04-21 23:36:14 +0530

    Added: Welcome Address Book program

---
+++ src/main/java/addressBookStream_Day24/Welcome.java
@@ -0,0 +1,9 @@
+package addressBookStream_Day24;
+
+public class Welcome {
+
+	public static void main(String[] args) {
+	System.out.println("Welcome to Address Book Using Java Stream API");
+	}
+
+}
